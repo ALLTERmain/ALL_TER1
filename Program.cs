e5e692e1de9f9ed22c5cb7903b02ac1478a8e1d5
ALLTERmain <shurik280208@gmail.com>
2023-05-31 20:30:44 +0400

    C1.3

--- Program.cs
+++ Program.cs
@@ -1,5 +1,24 @@
-﻿int a=Convert.ToInt32(Console.ReadLine());
-for (int i = 0; i < a; i++)
+﻿
+for (int i = 20; i < 36; i++)
 {
-    Console.Write($"45 ");
+    Console.Write($"{i} ");
+}
+Console.Write("\n");
+int b=Convert.ToInt32(Console.ReadLine());
+for (int i = 10; i < b; i++)
+{
+    Console.Write($"{i * i} ");
+}
+Console.Write("\n");
+int c = Convert.ToInt32(Console.ReadLine());
+for (int i = c; i < 51; i++)
+{
+    Console.Write($"{i * i * i} ");
+}
+Console.Write("\n");
+int x = Convert.ToInt32(Console.ReadLine());
+int y = Convert.ToInt32(Console.ReadLine());
+for (int i = x; i < y; i++)
+{
+    Console.Write($"{i} ");
 }
