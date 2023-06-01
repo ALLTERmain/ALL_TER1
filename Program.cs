fcb4be4ad5c4474f5984f20efdce479e4f343a7d
ALLTERmain <shurik280208@gmail.com>
2023-06-01 20:21:45 +0400

    C2.1

--- Program.cs
+++ Program.cs
@@ -1,24 +1,6 @@
-﻿
-for (int i = 20; i < 36; i++)
+﻿int n = 0;
+for  (int i = 100; i < 501; i++)
 {
-    Console.Write($"{i} ");
+    n += i;
 }
-Console.Write("\n");
-int b=Convert.ToInt32(Console.ReadLine());
-for (int i = 10; i < b; i++)
-{
-    Console.Write($"{i * i} ");
-}
-Console.Write("\n");
-int c = Convert.ToInt32(Console.ReadLine());
-for (int i = c; i < 51; i++)
-{
-    Console.Write($"{i * i * i} ");
-}
-Console.Write("\n");
-int x = Convert.ToInt32(Console.ReadLine());
-int y = Convert.ToInt32(Console.ReadLine());
-for (int i = x; i < y; i++)
-{
-    Console.Write($"{i} ");
-}
+Console.WriteLine(n);
