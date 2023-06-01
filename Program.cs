19b6ce851ad8ff8a7bd68d952c863c5209447045
ALLTERmain <shurik280208@gmail.com>
2023-06-01 20:29:51 +0400

    C2.1 d

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿int n = 0;
 int a=Convert.ToInt32(Console.ReadLine());
-for  (int i = -10; i < a; i++)
+int b = Convert.ToInt32(Console.ReadLine());
+for  (int i = a; i < b+1; i++)
 {
     n += i;
 }
